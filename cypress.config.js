00cd23ad415397a5850cde480f3982719b95c1c2
SylwiaK-H <sylwiakielb@gmail.com>
2026-04-24 07:48:33 +0200

    Delete outdated Cypress test files and add a new test for the main page navigation

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
+  viewportHeight: 800,
+  viewportWidth: 1280,
 
   e2e: {
     setupNodeEvents(on, config) {
